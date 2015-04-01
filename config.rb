c2751d34dfbd6871a09f6a2f124d1a1e92cf52c6
Ben Lenarts <ben@tupil.com>
2015-04-01 15:05:31 +0700

    Optimize other images using imageoptim.

--- config.rb
+++ config.rb
@@ -53,6 +53,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+activate :imageoptim
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -69,4 +71,5 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+
 end
